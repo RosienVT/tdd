55072c62e092f0e74f76d935cb19c680a221fa19
RosienVT <rosien.vantoor@gmail.com>
2016-06-21 16:45:05 +0200

    Fixed error with selenium, more functional and unit test. Commit before refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -15,5 +15,22 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', request=request) # added to fix unexpected error re csrf token
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+            request = HttpRequest()
+            request.method = 'POST'
+            request.POST['item_text'] = 'A new list item'
+
+            response = home_page(request)
+
+            # code below: request = request is to fix the unexpected error triggered by a csrf token issue in django 1.9
+            expected_html = render_to_string(
+                'home.html',
+                {'new_item_text': 'A new list item'},
+                request=request
+            )
+            self.assertEqual(response.content.decode(), expected_html)
+
+

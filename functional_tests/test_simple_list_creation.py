1f0c21bd97ded61c826356b9478cc11da1f7c05a
RosienVT <rosien.vantoor@gmail.com>
2016-07-04 15:49:48 +0200

    Fixed tests that were still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,11 +1,9 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
 from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
